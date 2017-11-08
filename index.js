a8a7d47be66a330063cce2b1123f03ef9c158184
sarahheacock <seheacock@bellsouth.net>
2017-11-08 15:55:26 -0500

    ghjk

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
+
 
 // Add headers
 app.use((req, response, next) => {
