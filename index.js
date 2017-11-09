b0f4ec967b365f9f43ffe1dec4d18d21fd30aad8
sarahheacock <seheacock@bellsouth.net>
2017-11-09 17:29:35 -0500

    fghjk

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
-const bodyParser = require('body-parser');
+// const request = require('request');
+// const cheerio = require('cheerio');
+// const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json'}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: 'application/json'}));
 
 
 // Add headers
 app.use((req, response, next) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  response.setHeader("Access-Control-Allow-Methods", "GET");
   response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
 //===============ROUTES====================================
-
+//===============create tree===============================
 // app.get('/scrape', (req, res, next) => {
-//   let result = [];
+//   let result = {
+//     tree: {},
+//     map: {}
+//   };
+//   //let urls = [];
 //
 //   const scrape = (url) => {
 //     request(url, (error, response, html) => {
@@ -32,17 +36,20 @@ app.use((req, response, next) => {
 //         const $ = cheerio.load(html);
 //
 //         const div = $('body').find('div.quick-links').find('a');
-//         result = div.map(function(){
+//         div.each(function(){
 //           const href = $(this).attr('href');
 //           const name = $(this).text();
 //
-//           if(href.includes('Global_Objects') && ((name >= 'Array' && name <= 'RegExp') || (name <= 'Set' && name >= 'WeakSet'))){
-//             return {
+//           if(href.includes('Global_Objects') && ((name >= 'Array' && name <= 'RegExp') || (name >= 'Set' && name <= 'WeakSet'))){
+//             const info = $(this).attr('title');
+//
+//             result['map'][name] = {
 //               href: "https://developer.mozilla.org" + href,
-//               name: name
-//             };
+//               info: info
+//             }
+//             //urls.push(href);
 //           }
-//         }).get();
+//         });
 //       }
 //     });
 //   }
@@ -50,115 +57,118 @@ app.use((req, response, next) => {
 //   scrape("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects", []);
 //
 //   const myFunc = () => {
-//     const done = result.length > 0;
+//     const done = result.map.hasOwnProperty('WeakSet');
 //     if(!done){
 //       setTimeout(myFunc, 1500);
 //     }
 //     else{ //DONE!
 //       req.result = result;
-//       console.log(req.result);
+//       //req.urls = urls;
+//
+//       // res.json(req.result);
 //       next();
 //     }
 //   }
-//
 //   myFunc();
 //
 // }, (req, res, next) => {
-//   let more = [];
 //
 //   const scrape = (url, n) => {
+//     //console.log(url);
 //     //console.log(url);
 //     request(url, (error, response, html) => {
 //       if(!error){
 //         const $ = cheerio.load(html);
-//
 //         const div = $('body').find('div.quick-links').find('a');
 //
-//         let result = div.map(function(){
+//         div.each(function(){
 //           const href = $(this).attr('href');
 //           const name = $(this).find('code').text();
-//           //console.log(href, name);
+//           console.log(href, name);
 //
 //           if(href.includes('Global_Objects') && name.includes(n)){
-//             //console.log(href, name);
-//             return {
-//               href: "https://developer.mozilla.org" + href,
-//               name: name
-//             };
-//           }
-//         }).get();
+//             console.log(href, name);
+//             const info = $(this).attr('title');
 //
-//         more = more.concat(result);
+//             req.result.map[name] = {
+//               href: "https://developer.mozilla.org" + href,
+//               info: info
+//             }
+//           }
+//         });
+//
 //       }
 //     });
 //   }
 //
-//   req.result.forEach((r) => {
-//     scrape(r.href, r.name);
+//   Object.keys(req.result.map).forEach((key) => {
+//     scrape(req.result['map'][key]["href"], key);
 //   });
 //
 //   const myFunc = () => {
-//     const done = more.length >= 384;
+//     const done = req.result.map.hasOwnProperty('WeakSet.prototype.has()');
+//     //console.log(req.result.map)
 //
 //     if(!done){
 //       setTimeout(myFunc, 1500);
 //     }
 //     else { //DONE!
-//       req.result = req.result.concat(more);
-//       console.log(req.result);
+//       //res.json(req.result);
+//       // req.result = req.result.concat(more);
+//       // console.log(req.result);
 //       next();
 //     }
 //   }
 //   myFunc();
 //
 // }, (req, res, next) => {
-//   let root = {};
+//   //let root = {};
 //
-//   req.result.forEach((obj) => {
+//   (Object.keys(req.result.map)).forEach((name) => {
 //     const create = (word) => {
 //       let letters = word.split('');
-//       let current = root;
+//       let current = req.result.tree;
 //
 //       letters.forEach((letter) => {
 //         if(!current.hasOwnProperty(letter)){
 //           current[letter] = {};
 //         }
+//
 //         current = current[letter];
-//       });
 //
-//       if(!current.hasOwnProperty("*")){
-//         current["*"] = [obj];
-//       }
-//       else {
-//         const valid = current["*"].reduce((a, b) => {
-//           return a && obj.name !== b.name;
-//         }, true);
-//
-//         if(valid){
-//           current["*"].push(obj);
+//         if(!current.hasOwnProperty('*')){
+//           current["*"] = [name];
 //         }
-//       }
+//         else {
+//           if(!current["*"].includes(name)){
+//             current["*"].push(name);
+//           }
+//         }
+//       });
 //     }
 //
-//     const words = obj.name.split('').map((letter, i) => {
-//         return obj.name.toLowerCase().slice(i);
+//     const words = name.split('').map((letter, i) => {
+//         return name.toLowerCase().slice(i);
 //     });
 //
 //     words.forEach((word) => {
 //       create(word);
 //     });
 //   });
-//   //res.json(root);
-//   fs.writeFile('output.json', JSON.stringify(root), (err) => {
+//
+//   // res.json(req.result);
+//   fs.writeFile('output.json', JSON.stringify(req.result), (err) => {
 //     console.log('File successfully written! - Check your project directory for the output.json file');
 //     res.json({done: true});
 //   });
 // });
-app.get('/word', (req, res, next) => {
-  fs.readFile('output.json', (err, data) => {
-    res.json(JSON.parse(data));
-  });
-});
+
+// =================retrieve=========================================
+// app.get('/word', (req, res, next) => {
+//   fs.readFile('output.json', (err, data) => {
+//     res.json(JSON.parse(data));
+//   });
+// });
 
 app.get('/:word', (req, res, next) => {
   fs.readFile('output.json', (err, d) => {
@@ -166,9 +176,8 @@ app.get('/:word', (req, res, next) => {
       next(err);
     }
     let data = JSON.parse(d);
-    let current = data;
+    let current = data.tree;
     let arr = req.params.word.replace("%20", " ").split('');
-    let result = [];
 
     arr.forEach((a) => {
       if(!current.hasOwnProperty(a)){
@@ -179,27 +188,37 @@ app.get('/:word', (req, res, next) => {
       }
     });
 
+    if(current.hasOwnProperty("*")){
+      let result = current["*"].map((name) => {
+        return Object.assign(data["map"][name], {name: name});
+      });
+      res.json(result);
+    }
+    else {
+      res.json([]);
+    }
+
     //result = current["*"].slice(0, 3);
 
-    let keys = Object.keys(current);
-
-    while(result.length < 5 && keys.length > 0){
-      if(keys.includes("*")){
-        result = result.concat(current["*"].slice(0, 3));
-        if(keys.length > 1){
-          current = current[keys[1]];
-        }
-        else {
-          break;
-        }
-      }
-      else {
-        current = current[keys[0]];
-      }
-
-      keys = Object.keys(current);
-    }
-    res.json(result);
+    // let keys = Object.keys(current);
+    //
+    // while(result.length < 5 && keys.length > 0){
+    //   if(keys.includes("*")){
+    //     result = result.concat(current["*"].slice(0, 3));
+    //     if(keys.length > 1){
+    //       current = current[keys[1]];
+    //     }
+    //     else {
+    //       break;
+    //     }
+    //   }
+    //   else {
+    //     current = current[keys[0]];
+    //   }
+    //
+    //   keys = Object.keys(current);
+    // }
+    // res.json(result);
   });
 });
 
